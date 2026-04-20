14925ebfd8711daeb3e4777c37a8aaab0a423b78
PaulixAlon <mjsafr951753@gmail.com>
2026-04-20 13:41:09 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+package Empregado;
+
+public class Main {
+    /**
+     * @see  https://refactoring.guru/move-method
+     */
+    public static void main(String[] args) {
+
+        // Crea un obxecto da clase CargoEmpregado para cada tipo de cargo
+
+        // Crea un obxecto da clase Empregado asignándolles o cargo correspondente
+
+        // Faino de tal xeito que deas executado:
+
+        System.out.println(empregado1.getNome() + " ten un soldo de " + empregado1.getSoldo());
+        System.out.println(empregado2.getNome() + " ten un soldo de " + empregado2.getSoldo());
+        System.out.println(empregado3.getNome() + " ten un soldo de " + empregado3.getSoldo());
+
+        //Comenta todo o código
+
+    }
+}
